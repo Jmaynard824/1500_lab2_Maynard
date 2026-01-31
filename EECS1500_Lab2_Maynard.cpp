00e814f1c5428d698ad3d9524074037873fbd9cc
Jmaynard824 <jmaynar6@rockets.utoledo.edu>
2026-01-30 21:24:59 -0500

    Add files via upload

---
+++ EECS1500_Lab2_Maynard.cpp
@@ -0,0 +1,18 @@
+//
+// Program: EECS 1500
+// Author: James Maynard
+// Date: 1/30/26
+// 
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+
+	cout << "Hello World! This is Maynard.\n" << endl;
+
+	return 0;
+}
